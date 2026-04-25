d95b06bb4a136e966f20e1a7a7c4de5f2475d37b
Rodrigo Almeida Prado <rodrigo.almprado@gmail.com>
2026-04-24 21:33:02 -0300

    Select yes no effect

---
+++ YgoSoul/Message/SelectEffectYesNoMessage.cs
@@ -0,0 +1,36 @@
+﻿using YgoSoul.Message.Abstr;
+using YgoSoul.Message.Component;
+using YgoSoul.Message.Enum;
+
+namespace YgoSoul.Message;
+
+public class SelectEffectYesNoMessage : IMessage
+{
+    public InputType Input => InputType.Value;
+    public int InputCount => 1;
+    public byte Player { get; }
+    public CardReference Card { get; }
+    public ulong Description { get; }
+
+    public SelectEffectYesNoMessage(byte player, CardReference card, ulong description)
+    {
+        Player = player;
+        Card = card;
+        Description = description;
+    }
+    
+    public byte[] GetResponse(int id)
+    {
+        if (id != 0 && id != 1)
+        {
+            return [0xFF, 0xFF, 0xFF, 0xFF];
+        }
+
+        return BitConverter.GetBytes(id);
+    }
+
+    public override string ToString()
+    {
+        return $"\nCard effect for card {CardLibrary.GetCard(Card.CardCode).Name}. 0 - No, 1 - Yes";
+    }
+}
---
+++ YgoSoul/Parser/SelectEffectYesNoParser.cs
@@ -0,0 +1,26 @@
+﻿using YgoSoul.Flag;
+using YgoSoul.Message;
+using YgoSoul.Message.Abstr;
+using YgoSoul.Message.Component;
+using YgoSoul.Parser.Abstr;
+using YgoSoul.Util;
+
+namespace YgoSoul.Parser;
+
+public class SelectEffectYesNoParser : BaseParser
+{
+    protected override IMessage DoParse(byte[] buffer)
+    {
+        var reader = new PacketReader(buffer);
+        reader.ReadByte();//msg
+        var player = reader.ReadByte();
+        var cardCode = reader.ReadUInt32();
+        var controller = reader.ReadByte();
+        var location = (CardLocation)reader.ReadByte();
+        var sequence = reader.ReadUInt32();
+        var position = (CardPosition) reader.ReadUInt32();
+        var description = reader.ReadUInt64();
+        var card = new CardReference(cardCode, controller, location, sequence, position, 0);
+        return new SelectEffectYesNoMessage(player, card, description);
+    }
+}
